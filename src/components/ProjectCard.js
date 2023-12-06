89c468d006fa5daf3547978ea5643f297c3e1af8
Abdur Rahman <abdurrahman27485@gmail.com>
2023-12-06 22:42:44 +0600

    work

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,24 +3,30 @@ import React from 'react';
 import Tag from './Tag';
 import { Link } from 'lucide-react';
 
-const ProjectCard = () => {
+const ProjectCard = ({ name, description, technologies, liveLink, thumb }) => {
     return (
         <div className='shadow-projectCard rounded-xl md:flex justify-between items-center bg-gray-50'>
-            <div className='p-8 md:p-12 rounded-xl'>
-                <Image src={'/me.jpg'} width={1000} height={1000} className='rounded-xl shadow-projectImg'></Image>
+            <div className='p-8 md:p-12 md:w-1/2 rounded-xl'>
+                <Image src={thumb} width={1000} height={1000} className='rounded-xl shadow-projectImg'></Image>
             </div>
-            <div className='p-8 md:p-12 space-y-6'>
-                <h4 className='text-gray-900 text-[18px] font-semibold'>Title</h4>
-                <p className='text-gray-600 text-[16px]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.</p>
+            <div className='p-8 md:p-12 space-y-6 md:w-1/2'>
+                <h4 className='text-gray-900 text-[18px] font-semibold'>{name}</h4>
+                <p className='text-gray-600 text-[16px]'>{description}</p>
                 <div className='flex items-center gap-2 flex-wrap'>
-                    <Tag text={'HTML'}></Tag>
-                    <Tag text={'CSS'}></Tag>
-                    <Tag text={'JavaScript'}></Tag>
-                    <Tag text={'Node.js'}></Tag>
-                    <Tag text={'Express.js'}></Tag>
-                    <Tag text={'React.js'}></Tag>
+                    {
+                        technologies ?
+                            technologies.map(tech => {
+                                return (
+                                    <Tag text={tech}></Tag>
+                                )
+                            })
+                            :
+                            ''
+                    }
                 </div>
-                <Link size={24}/>
+                <Link href={liveLink}>
+                    <Link size={24} />
+                </Link>
             </div>
         </div>
     );
--- src/components/sections/Work.js
+++ src/components/sections/Work.js
@@ -5,6 +5,36 @@ import Tag from '../Tag';
 import ProjectCard from '../ProjectCard';
 
 const Work = () => {
+    const projects = [
+        {
+            name: 'Fractos',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
+            liveLink: 'https://fractos-client.vercel.app/',
+            thumb: '/projectThumbs/fractos.png'
+        },
+        {
+            name: 'Triolingo',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
+            liveLink: 'https://fractos-client.vercel.app/',
+            thumb: '/projectThumbs/triolingo.png'
+        },
+        {
+            name: 'Toygo',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
+            liveLink: 'https://fractos-client.vercel.app/',
+            thumb: '/projectThumbs/toygo.png'
+        },
+        {
+            name: 'Food Hunt',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
+            liveLink: 'https://fractos-client.vercel.app/',
+            thumb: '/projectThumbs/foodHunt.png'
+        }
+    ]
     return (
         <SectionContainer bg={'bg-gray-100'} id={'work'}>
             <div className='flex justify-center items-center'>
@@ -12,10 +42,17 @@ const Work = () => {
             </div>
             <SectionSecondaryText text={'Some of the noteworthy projects I have built:'}></SectionSecondaryText>
             <div className='mt-12 space-y-6'>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
+                {
+                    projects ?
+                        projects.map(project => {
+                            const { name, description, technologies, liveLink, thumb } = project;
+                            return (
+                                <ProjectCard name={name} description={description} technologies={technologies} liveLink={liveLink} thumb={thumb}></ProjectCard>
+                            )
+                        })
+                        :
+                        ''
+                }
             </div>
         </SectionContainer>
     );
