6d7e3e65f72c5b7d64090b8ab498dcdbf6bd72f2
Abdur Rahman <abdurrahman27485@gmail.com>
2023-12-10 21:18:39 +0600

    changed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Alert = ({ display }) => {
     return (
-        <div className={`${display ? 'lg:fixed hidden' : 'hidden'} top-3/4 p-2 px-4 rounded-full left-1/2 -translate-x-1/2 bg-gray-200 bg-opacity-50 text-gray-900`}>
+        <div className={`${display ? 'md:fixed hidden' : 'hidden'} top-3/4 p-2 px-4 rounded-full left-1/2 -translate-x-1/2 bg-gray-200 bg-opacity-50 text-gray-900`}>
             <p className='text-base'>Text Copied</p>
         </div>
     );
