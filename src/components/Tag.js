f94d9ba003ddac3da0e02a06c708c35bb8a4a2aa
Abdur Rahman <abdurrahman27485@gmail.com>
2023-12-12 22:01:33 +0600

    update

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Tag = ({ text, type }) => {
     return (
-            <div className={`px-5 py-1 ${type == 'tag' ? 'bg-gray-200 text-gray-600' : 'bg-slate-200 text-gray-900 font-bold shadow-inner'}  font-medium text-center rounded-full`}>
+            <div className={`px-5 py-1 ${type == 'tag' ? 'bg-gray-200 text-gray-600' : 'bg-slate-900 text-white font-bold shadow-inner'}  font-medium text-center rounded-full`}>
                 <span>{text}</span>
             </div>
     );
--- src/components/sections/Work.js
+++ src/components/sections/Work.js
@@ -3,38 +3,59 @@ import SectionContainer from '../SectionContainer';
 import SectionSecondaryText from '../SectionSecondaryText';
 import Tag from '../Tag';
 import ProjectCard from '../ProjectCard';
+import Dialog from '../Dialog';
 
 const Work = () => {
     const projects = [
         {
             name: 'Fractos',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
-            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
+            description: [
+                'Students evaluate their learning using features mock exam, performance analysis, question archive and a leaderboard to track their peers score.',
+                'Blog posts feature to improve knowledge and get to know about new and exciting things everyday.',
+                'Teachers expand their ways to engage with their students using feature like batch and course management system.'
+            ],
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript','MongoDB','Mongoose','NextJS','TailwindCSS','DaisyUI','StripeJS','JWT','Firebase'],
             liveLink: 'https://fractos-client.vercel.app/',
             thumb: '/projectThumbs/fractos.png',
-            constribution: 'Frontend'
+            contribution: ['Frontend', 'Project Management', 'UI/UX']
         },
         {
             name: 'Triolingo',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
-            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
-            liveLink: 'https://fractos-client.vercel.app/',
+            description: [
+                'Instructors can share their knowledge through class management feature. Create, Delete, Update classes from dashboard.',
+                'Admin dashboard to manage student and instructors and the contents of the site.',
+                'Secure payment system and users are authorized using technology like JWT.'
+            ],
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript','MongoDB','TailwindCSS','JWT','Axios','Firebase'],
+            liveLink: 'https://triolingo-27485.web.app/',
             thumb: '/projectThumbs/triolingo.png',
-            constribution: 'Frontend'
+            contribution: ['Frontend', 'ExpressJS API', 'Database']
         },
         {
             name: 'Toygo',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
-            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
-            liveLink: 'https://fractos-client.vercel.app/',
-            thumb: '/projectThumbs/toygo.png'
+            description: [
+                'Buy car from different categories and read our customers feedbacks.',
+                'Seller accounts can add, remove or update their product inventory.',
+                'Admin account can manage users, inventory and contents of the site.'
+            ],
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript','MongoDB','TailwindCSS','Firebase','DaisyUI'],
+            liveLink: 'https://toygo-27485.web.app/',
+            thumb: '/projectThumbs/toygo.png',
+            contribution: ['Frontend', 'ExpressJS API', 'Database']
         },
         {
             name: 'Food Hunt',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus debitis, reiciendis fugit quis ipsum tenetur alias, cumque mollitia aut, nemo recusandae dolores quibusdam necessitatibus distinctio! Nesciunt asperiores minus recusandae reiciendis.',
-            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript', 'CSS', 'HTML'],
-            liveLink: 'https://fractos-client.vercel.app/',
-            thumb: '/projectThumbs/foodHunt.png'
+            description: [
+                'Find best chefs of Indian cuisine.',
+                'Detailed cooking method.',
+                'Interactive ui for your convenience.',
+                'Authentic Information.',
+                'All the recipes are tasty as well as healthy too.'
+            ],
+            technologies: ['Express', 'React.js', 'Node.js', 'JavaScript','MongoDB','TailwindCSS','Firebase','DaisyUI'],
+            liveLink: 'https://food-hunt27485.netlify.app/',
+            thumb: '/projectThumbs/foodHunt.png',
+            contribution: ['Frontend', 'ExpressJS API', 'Database']
         }
     ]
     return (
@@ -47,9 +68,9 @@ const Work = () => {
                 {
                     projects ?
                         projects.map(project => {
-                            const { name, description, technologies, liveLink, thumb } = project;
+                            const { name, description, technologies, liveLink, thumb, contribution } = project;
                             return (
-                                <ProjectCard name={name} description={description} technologies={technologies} liveLink={liveLink} thumb={thumb}></ProjectCard>
+                                <ProjectCard name={name} description={description} technologies={technologies} liveLink={liveLink} thumb={thumb} contribution={contribution}></ProjectCard>
                             )
                         })
                         :
