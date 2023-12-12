ff6deed06775e32dad43d235498ab29a51a333af
Abdur Rahman <abdurrahman27485@gmail.com>
2023-12-12 22:01:23 +0600

    update

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,23 +1,42 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Tag from './Tag';
-import { Link } from 'lucide-react';
+import { InfoIcon, Link } from 'lucide-react';
+import Dialog from './Dialog';
 
 const ProjectCard = ({ name, description, technologies, liveLink, thumb, contribution }) => {
+    const [dialogShow, setDialogShow] = useState(false);
     return (
         <div className='shadow-projectCard rounded-xl md:flex justify-between items-center bg-gray-50'>
             <div className='p-8 md:p-12 md:w-1/2 rounded-xl'>
-                <Image src={thumb} width={1000} height={1000} className='rounded-xl shadow-projectImg'></Image>
+                <img src={thumb} className={`rounded-xl  w-full shadow-projectImg`}></img>
             </div>
             <div className='p-8 md:p-12 space-y-6 md:w-1/2'>
-                <h4 className='text-gray-900 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold'>{name}</h4>
+                <h4 className='text-gray-900 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold flex justify-between items-center'>
+                    <span>{name}</span>
+                    <InfoIcon onClick={() => setDialogShow(true)} size={56} cursor={'pointer'} className='hover:border-gray-600 transition-all duration-200 rounded-full border-white border-8 '></InfoIcon>
+                </h4>
                 <div className='my-4'>
                     <h3 className='text-xl text-gray-900 md:text-2xl font-semibold'>Worked On</h3>
-                    <div className='flex items-center mt-2'>
-                        <Tag text={contribution} type={''}></Tag>
+                    <div className='flex items-center mt-2 gap-2'>
+                        {contribution?.map((text) => {
+                            console.log(text)
+                            return (
+                                <Tag text={text} type={''}></Tag>
+                            )
+                        })}
                     </div>
                 </div>
-                <p className='text-gray-600 text-[16px]'>{description}</p>
+                {/* <div className='text-gray-600 text-base md:text-xl flex gap-y-4 flex-col flex-nowrap'>
+                    {description.map(item => {
+                        return (
+                            <p className='flex items-top gap-2'>
+                                <Image className=' self-start' src={'/bullet-point 24.png'} width={24} height={24}></Image>
+                                {item}
+                            </p>
+                        )
+                    })}
+                </div> */}
                 <div className='flex items-center gap-2 flex-wrap'>
                     {
                         technologies ?
@@ -30,10 +49,11 @@ const ProjectCard = ({ name, description, technologies, liveLink, thumb, contrib
                             ''
                     }
                 </div>
-                <Link href={liveLink}>
+                <Link target='_blank' href={liveLink}>
                     <Link size={24} />
                 </Link>
             </div>
+            <Dialog display={dialogShow} setDialogShow={setDialogShow} content={description}></Dialog>
         </div>
     );
 };
